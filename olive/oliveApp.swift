f1c9959a9f747ad3f99703d5674f1f390cd471cb
Michael Christie <christie.michael@gmail.com>
2024-06-25 15:44:22 +1200

    Initial Commit

---
+++ olive/oliveApp.swift
@@ -0,0 +1,17 @@
+//
+//  oliveApp.swift
+//  olive
+//
+//  Created by Michael Christie on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct oliveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
