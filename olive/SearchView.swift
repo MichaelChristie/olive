97219d409877fa35b3868edb39250670343781db
Michael Christie <christie.michael@gmail.com>
2024-06-26 20:14:04 +1200

    simple UI add

--- olive/SearchView.swift
+++ olive/SearchView.swift
@@ -11,28 +11,20 @@ struct SearchView: View {
     @State private var searchText = ""
     
     var body: some View {
-        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-        }
-        Gauge(value: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*/0...1/*@END_MENU_TOKEN@*/) {
-            /*@START_MENU_TOKEN@*/Text("Label")/*@END_MENU_TOKEN@*/
-        }
-        Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
-            /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
-            /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
-        }
-        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
-            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
-        }
 
-        TextEditor(text: /*@START_MENU_TOKEN@*/.constant("Placeholder")/*@END_MENU_TOKEN@*/)
-        Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
-        
-        ShareLink(item: /*@START_MENU_TOKEN@*/URL(string: "https://developer.apple.com/xcode/swiftui")!/*@END_MENU_TOKEN@*/)
-        ScrollView {
-            Image("Salad")
+
+
+        Button(action: {}) {
+          Image(systemName: "magnifyingglass")
+          Text("Search")
+            .padding(.horizontal)
         }
+        .padding()
+        .foregroundColor(.white)
+        .background(.orange)
+        .cornerRadius(10)
+        
+        
 
         TextField("Enter ingredient", text: $searchText)
             .padding()
